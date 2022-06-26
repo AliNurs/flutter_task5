2f64d6a3aa32d9c459db353c27fb239d17e33d4c
Nurs <nnurs.kgg@gmail.com>
2022-06-26 11:32:21 +0600

    The best Practice on flutter, Screen 5

---
+++ lib/widgets/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String bg1 = 'assets/images/bg1.png';
+  static const String bg2 = 'assets/images/bg2.png';
+  static const String barter = 'assets/images/barter.png';
+  static const String list = 'assets/images/list.png';
+  static const String molniya = 'images/molniya.png';
+  static const String reklama = 'assets/images/reklama.png';
+  static const String vzaimopiar = 'assets/images/vzaimopiar.png';
+}
